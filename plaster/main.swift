6b18c84a7ee783c461d176bbe31c111313abe702
Quinn McHenry <quinn@jqm.us>
2015-12-15 08:54:11 -0500

    Initial Commit

---
+++ plaster/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  plaster
+//
+//  Created by Quinn McHenry on 12/15/15.
+//  Copyright © 2015 Quinn McHenry. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

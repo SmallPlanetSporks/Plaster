a459ac130126cf3d2dfc3649ddf3e59a13a5fd6e
Quinn McHenry <quinn@jqm.us>
2017-03-27 17:32:29 -0400

    Update FileKit ref for XC 8.3 compatibility

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "plaster",
     dependencies: [
-	.Package(url: "https://github.com/qmchenry/FileKit.git", Version(4, 0, 0, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "1")),
+	.Package(url: "https://github.com/qmchenry/FileKit.git", Version(4, 0, 2, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "1")),
         .Package(url: "https://github.com/jatoben/CommandLine.git", Version(3, 0, 0, prereleaseIdentifiers: ["pre"], buildMetadataIdentifier: "1")),
     ]
 )
